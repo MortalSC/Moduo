f35a34700d63a4d3179538275444528bd88c8127
MortalSoul&Code <Mortal_SCode@163.com>
2024-02-10 20:53:40 +0800

    InetAddress类封装 ip/port 模块

---
+++ MuduoCoding/Muduo/InetAddress.h
@@ -0,0 +1,33 @@
+#pragma once
+
+/**
+ * 封装 socket 所需的 ip / port
+*/
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+
+namespace NetWork{
+    class InetAddress{
+    public:
+    // 构造函数
+        explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
+        explicit InetAddress(const sockaddr_in & addr): addr_(addr){}
+
+    // 其他接口：获取 ip / port
+        // 获取 ip
+        std::string toIp() const;
+        // 获取 ip 和 端口号
+        std::string toIpPort() const;
+        // 获取端口号
+        uint16_t toPort() const;
+        // 获取对象
+        const sockaddr_in* getSockAddr() const
+        {
+            return &addr_;
+        }
+    private:
+        struct sockaddr_in addr_;
+    };
+};
