cbcfa69ed1c43165aedef21ef56dc3e2cc1ac49a
MortalSoul&Code <Mortal_SCode@163.com>
2024-02-10 20:54:32 +0800

    build

---
+++ MuduoCoding/build/CMakeFiles/mymuduo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mymuduo.dir/InetAddress.o"
+  "CMakeFiles/mymuduo.dir/Log.o"
+  "CMakeFiles/mymuduo.dir/Timestamp.o"
+  "/home/Mortal/MuduoProject/Muduo/dir/libmymuduo.pdb"
+  "/home/Mortal/MuduoProject/Muduo/dir/libmymuduo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mymuduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
