9ab9432d9794e325372cf3fd30e900074ef4b992
MortalSoul&Code <Mortal_SCode@163.com>
2024-02-15 19:22:49 +0800

    命名空间调整

--- MuduoCoding/Muduo/InetAddress.h
+++ MuduoCoding/Muduo/InetAddress.h
@@ -9,30 +9,30 @@
 #include <string>
 namespace myMuduo
 {
-    namespace NetWork
+    // namespace NetWork
+    // {
+    class InetAddress
     {
-        class InetAddress
+    public:
+        // 构造函数
+        explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
+        explicit InetAddress(const sockaddr_in &addr) : addr_(addr) {}
+
+        // 其他接口：获取 ip / port
+        // 获取 ip
+        std::string toIp() const;
+        // 获取 ip 和 端口号
+        std::string toIpPort() const;
+        // 获取端口号
+        uint16_t toPort() const;
+        // 获取对象
+        const sockaddr_in *getSockAddr() const
         {
-        public:
-            // 构造函数
-            explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
-            explicit InetAddress(const sockaddr_in &addr) : addr_(addr) {}
+            return &addr_;
+        }
 
-            // 其他接口：获取 ip / port
-            // 获取 ip
-            std::string toIp() const;
-            // 获取 ip 和 端口号
-            std::string toIpPort() const;
-            // 获取端口号
-            uint16_t toPort() const;
-            // 获取对象
-            const sockaddr_in *getSockAddr() const
-            {
-                return &addr_;
-            }
-
-        private:
-            struct sockaddr_in addr_;
-        };
-    }
+    private:
+        struct sockaddr_in addr_;
+    };
+    // }
 }
