63393734ad9b450697cf4b365be6a1e6e976a1da
MortalSoul&Code <Mortal_SCode@163.com>
2024-02-16 20:25:57 +0800

    初步实现TcpServer:对象构造联动Acceptor&EventLoopThreadPoll《实现服务端的启动

---
+++ MuduoCoding/Muduo/TcpServer.cc
@@ -0,0 +1,77 @@
+#include "TcpServer.h"
+#include "Log.h"
+
+#include <functional>
+
+namespace myMuduo
+{
+    EventLoop *CheckLoopNotNull(EventLoop *loop)
+    {
+        if (loop == nullptr)
+        {
+            Log_FATAL("%s:%s:%d mainLoop is Null!\n", __FILE__, __FUNCTION__, __LINE__);
+        }
+        return loop;
+    }
+    TcpServer::TcpServer(EventLoop *loop,
+                         const InetAddress &listenAddr,
+                         const std::string &name,
+                         Option option)
+        : loop_(CheckLoopNotNull(loop)), // 给定的 loop 不能为空！！！
+          ipPort_(listenAddr.toIpPort()),
+          name_(name),
+          acceptor_(new Acceptor(loop, listenAddr, option == kReusePort)), // mainLoop 上执行 Acceptor[创建fd、listen ...]
+          threadPool_(new EventLoopThreadPool(loop, name)),                // 创建线程池对象，默认只有主线程
+          connectionCallback_(),
+          messageCallback_(),
+          nextConnId_(1)
+    {
+        // 当有新用户连接时，会执行 TcpServer::newConnection 新连接回调
+        // 在代码上就是在 Acceptor::headleRead() 中去执行
+        acceptor_->setNewConnectionCallback(std::bind(&TcpServer::newConnection, this,
+                                                      std::placeholders::_1, std::placeholders::_2));
+    }
+    TcpServer::~TcpServer() {}
+
+    /* 设置底层 subLoop/线程 个数 */
+    void TcpServer::setThreadNum(int numThreads)
+    {
+        // 调用底层设置线程数量
+        // 之后，若调用 EventLoopThreadPool::start 就可以创建指定个数的线程
+        // 并加入 线程管理数组threads_ / loop 管理数组
+        threadPool_->setThreadNum(numThreads);
+    }
+
+    /* 开启服务器监听：就是开启 mainLoop的Acceptor的listen */
+    void TcpServer::start()
+    {
+
+        if (started_++ == 0)
+        { // 防止一个TcpServer对象被多次启动
+            // 启动线程池对象管理线程
+            // 会根据指定的线程个数创建线程并放入 threads_ & subLoop_ 进行管理
+            threadPool_->start(threadInitCallback_);
+            // 当前 mainLoop 通过调用 runInLoop
+            /**
+             * 1. 如果是 mainLoop 中执行，就会调用 Acceptor::listen
+             *  => 启动新用户连接监听
+             *  => 把监听到的新用户连接包装注册到 Poller，指定监听的事件行为
+             */
+            loop_->runInLoop(std::bind(&Acceptor::listen, acceptor_.get()));
+        }
+    }
+
+    void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr){
+
+    }
+
+    /* 删除连接：从 ConnectionMap 中删除 */
+    void TcpServer::removeConnection(const TcpConnectionPtr &conn){
+
+    }
+    /* 在 loop 中删除连接事件 */
+    void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn){
+        
+    }
+
+}
---
+++ MuduoCoding/Muduo/TcpServer.h
@@ -0,0 +1,88 @@
+#pragma once
+
+/**
+ * 提供服务器编程使用的类【对外提供服务器编程】
+ */
+
+#include "EventLoop.h"
+#include "Acceptor.h"
+#include "InetAddress.h"
+#include "noncopyable.h"
+#include "EventLoopThreadPool.h"
+#include "Callbacks.h"
+
+#include <functional>
+#include <string>
+#include <memory>
+#include <atomic>
+#include <unordered_map>
+
+namespace myMuduo
+{
+    class TcpServer : noncopyable
+    {
+    public:
+        using ThreadInitCallback = std::function<void(EventLoop *)>;
+        /* 用于标识端口是否可重用 */
+        enum Option
+        {
+            kNoReusePort,
+            kReusePort
+        };
+
+        TcpServer(EventLoop *loop,
+                  const InetAddress &listenAddr,
+                  const std::string& name = std::string(),
+                  Option option = kNoReusePort);
+        ~TcpServer();
+
+        /* 设置线程初始回调 */
+        void setThreadInitCallback(const ThreadInitCallback &cb) { threadInitCallback_ = cb; }
+
+        /* 设置新连接回调 */
+        void setConnectionCallback(const ConnectionCallback &cb) { connectionCallback_ = cb; }
+
+        /* 设置读写事件发生回调 */
+        void setMessageCallback(const MessageCallback &cb) { messageCallback_ = cb; }
+
+        /* 设置消息发送完成回调 */
+        void setWriteCompleteCallback(const WriteComleteCallback &cb) { writeComleteCallback_ = cb; }
+
+        /* 设置底层 subLoop/线程 个数 */
+        void setThreadNum(int numThreads);
+
+        /* 开启服务器监听：就是开启 mainLoop的Acceptor的listen */
+        void start();
+
+    private:
+        void newConnection(int sockfd, const InetAddress &peerAddr);
+        /* 删除连接：从 ConnectionMap 中删除 */
+        void removeConnection(const TcpConnectionPtr &conn);
+        /* 在 loop 中删除连接事件 */
+        void removeConnectionInLoop(const TcpConnectionPtr &conn);
+
+    private:
+        using ConnectionMap = std::unordered_map<std::string, TcpConnectionPtr>;
+
+        EventLoop *loop_;          // 关联 baseLoop
+        const std::string ipPort_; // 打包 ip和port
+        const std::string name_;
+
+        std::unique_ptr<Acceptor> acceptor_; // 在 mainLoop 中运行的 Acceptor，监听新用户连接事件
+
+        // 用户可以调用setThreadNum设置多个线程
+        std::shared_ptr<EventLoopThreadPool> threadPool_; // one loop per thread
+
+        ConnectionCallback connectionCallback_;     // 有新连接的回调
+        MessageCallback messageCallback_;           // 有读写消息时的回调
+        WriteComleteCallback writeComleteCallback_; // 消息发送完成的回调
+
+        ThreadInitCallback threadInitCallback_; // loop 线程初始化回调
+
+        std::atomic_int started_;
+
+        int nextConnId_;
+
+        ConnectionMap connections_; // 存储所有的连接
+    };
+}
