850b13e51e95fc10c1bc57588dddae4569adc3fa
MortalSoul&Code <Mortal_SCode@163.com>
2024-02-16 20:19:11 +0800

    TcpServer中回调类型的声明

---
+++ MuduoCoding/Muduo/Callbacks.h
@@ -0,0 +1,25 @@
+#pragma once
+/**
+ * 与 TcpConnection 管理的回调类型定义
+*/
+
+#include <memory>
+#include <functional>
+
+namespace myMuduo{
+    /* 前置类型 */
+    class Buffer;
+    class TcpConnection;
+    class Timestamp;
+    
+    /* 指向 TcpConnection 的智能指针 */
+    using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+    /* 关联 对端信息的回调函数 获取 */
+    using ConnectionCallback = std::function<void (const TcpConnectionPtr&)>;
+    /* 关闭 */
+    using closeCallback = std::function<void (const TcpConnectionPtr&)>;
+    /**/
+    using WriteComleteCallback = std::function<void (const TcpConnectionPtr&)>;
+    /**/
+    using MessageCallback = std::function<void (const TcpConnectionPtr&, Buffer*,Timestamp)>;
+}
